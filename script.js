9d5cfb6cae9d4f4941a23c1b1203aadead8ffdf2
Henry-pixel-dev <henrylaskid5@gmail.com>
2026-04-16 16:45:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+let task = JSON.parse(localStorage.getItem("task")) || [];
+
+window.addEventListener('load', () => {
+    task.forEach(taskObj => {
+        if (ul.childElementCount === 1) {
+            
+            const newText = taskObj.text;
+            addTask(newText);
+        }else {
+            return;
+        } 
+            
+    });    
+})
+
 const toggleBtn = document.getElementById('toggleBtn');
 const addBtn = document.getElementById('addBtn');
 const inputArea = document.getElementById('inputArea');
@@ -11,18 +26,20 @@ const toogleImg = document.getElementById('toogleImg');
 const filters = document.getElementById('filters');
 const drag = document.getElementById('drag');
 const display = document.getElementById('display');
+const completeImg = document.createElement('img');
+completeImg.src = 'images/icon-check.svg';
+completeImg.style.display = 'none'
 
-let currentTheme = 'light';
 
-toggleBtn.addEventListener('click', () => {
-    document.body.classList.toggle('dark');
-    currentTheme = currentTheme === 'light' ? 'dark' : 'light';
-    toogleImg.src = currentTheme === 'light' ? 'images/icon-moon.svg' : 'images/icon-sun.svg';
-});
+function addTask(text) { 
+    if (text == '') return; 
 
-addBtn.addEventListener('click', () => {
-    const inputValue = inputArea.value.trim(); 
-    if (inputValue == '') return; 
+
+    const id = Date.now() + Math.random();
+    const taskObj = {id, text, completed: false};
+    task.push(taskObj);
+    localStorage.setItem("task", JSON.stringify(task))
+
 
     const li = document.createElement('li');
     const divBox = document.createElement('div');
@@ -33,12 +50,13 @@ addBtn.addEventListener('click', () => {
     deleteImg.src = 'images/icon-cross.svg';
     const p = document.createElement('p');
 
+
     li.classList.add('li');
     divBox.classList.add('divBox');
     completeBtn.classList.add('complete-btn');
     p.classList.add('todo-text');
 
-    p.textContent = inputValue;
+    p.textContent = text;
 
     deleteBtn.appendChild(deleteImg);
     divBox.appendChild(completeBtn);
@@ -52,15 +70,91 @@ addBtn.addEventListener('click', () => {
     filters.classList.remove('hidden');
     display.classList.remove('hidden');
     drag.classList.remove('hidden');
+    filters.style.display = 'flex';
+    display.style.display = 'flex';
+    drag.style.display = 'flex'
+    
 
     completeBtn.addEventListener('click', () => {
-        const completeImg = document.createElement('img');
-        completeImg.src = 'images/icon-check.svg';
+        if (completeImg.style.display === 'none'){
+            completeImg.style.display ='block'
+        } else {
+            completeImg.style. display = 'none';
+        }
         completeBtn.appendChild(completeImg);
         completeImg.classList.add('todo-check-icon');
         completeBtn.classList.toggle('checked');
         p.classList.toggle('completed-text');
+
+        task = task.map(t => 
+        t.id === id ? { ...t, completed: !t.completed } : t
+        );
+        localStorage.setItem("task", JSON.stringify(task));
+        const users = localStorage.getItem("task")
+        console.log(users)
         updateItemsRemaining();
     });
 
+    deleteBtn.addEventListener('click', () => {
+        ul.removeChild(li)
+        task =  task.filter(t => t.id !== id)
+        console.log(task)
+        localStorage.removeItem("task")
+        const users = localStorage.getItem("task")
+        console.log(users)
+
+        if(ul.childElementCount === 1) {
+            filters.style.display = 'none';
+            display.style.display = 'none';
+            drag.style.display = 'none'
+        } else {
+            filters.style.display = 'flex';
+            display.style.display = 'flex';
+            drag.style.display = 'flex'
+        }
+    })
+}
+
+let currentTheme = 'light';
+
+toggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    currentTheme = currentTheme === 'light' ? 'dark' : 'light';
+    toogleImg.src = currentTheme === 'light' ? 'images/icon-moon.svg' : 'images/icon-sun.svg';
+});
+
+addBtn.addEventListener('click', () => {
+    const displaytext = inputArea.value.trim();
+    addTask(displaytext);
 })
+
+function updateItemsRemaining() {
+
+}
+
+
+function render(arrayToRender) {
+    ul.innerHTML = '';
+    arrayToRender.forEach(t => {
+        const li = document.createElement('li');
+        li.textContent = t.text
+        if(t.completed) li.classList.add('done');
+        ul.append(li)    
+    })
+}
+
+completedBtn.addEventListener('click', () => {
+   const completedTasks = task.filter(t => t.completed);
+render(completedTasks);
+
+})
+
+activeBtn.addEventListener('click', () => {
+    const pendingTasks = task.filter(t => !t.completed);
+    render(pendingTasks);
+
+})
+allBtn.addEventListener('click', () => {
+    render(task)
+})
+// localStorage.clear()
